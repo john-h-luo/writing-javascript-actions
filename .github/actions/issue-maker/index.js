45731f6aa1e64d875f90141b028c148aa3afebc9
john-h-luo <67673717+john-h-luo@users.noreply.github.com>
2022-03-21 00:09:54 +0800

    Update index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const octokit = require("@octokit/rest");
 
 async function run() {
   try {
@@ -7,9 +8,9 @@ async function run() {
     const jokeBody = core.getInput("joke");
     const token = core.getInput("repo-token");
 
-    const octokit = new github.getOctokit(token);
+    const issue = new octokit.getOctokit(token);
 
-    const newIssue = await octokit.issues.create({
+    const newIssue = await issue.issues.create({
       repo: github.context.repo.repo,
       owner: github.context.repo.owner,
       title: issueTitle,
